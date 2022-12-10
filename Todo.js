3309aa8f2f95bba907fa762de9abf670ac0b2f49
Eunyoung Lee <dmsdud22200@naver.com>
2022-12-11 01:25:24 +0900

    0.0.1

---
+++ Todo.js
@@ -0,0 +1,220 @@
+import { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,                                                         //컨텐츠가 아이폰 노치에 가려지지 않게해주는 View
+  View,
+  TextInput,
+  Text,
+  FlatList,                                                             //컴포넌트 리스트를 동적으로 화면에 보여주는 scrollview
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';             //Icon 라이브러리
+import AsyncStorage from '@react-native-async-storage/async-storage';   //비동기적 저장소, 앱이 꺼져도 데이터가 유지되는 장점
+const COLORS = { primary: '#1f145c', white: '#fff' };                  
+
+
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [textInput, setTextInput] = useState('');
+
+  useEffect(() => {
+    getTodosFromUserDevice();
+  }, []);
+
+  useEffect(() => {
+    saveTodoToUserDevice(todos);
+  }, [todos]);
+
+  const addTodo = () => {
+    if (textInput == '') {
+      Alert.alert('Error', 'Please input todo');
+    } else {
+      const newTodo = {
+        id: Math.random(),
+        task: textInput,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTextInput('');
+    }
+  };
+
+  const saveTodoToUserDevice = async todos => {
+    try {
+      const stringifyTodos = JSON.stringify(todos);
+      await AsyncStorage.setItem('todos', stringifyTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTodosFromUserDevice = async () => {
+    try {
+      const todos = await AsyncStorage.getItem('todos');
+      if (todos != null) {
+        setTodos(JSON.parse(todos));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const markTodoComplete = todoId => {
+    const newTodosItem = todos.map(item => {
+      if (item.id == todoId) {
+        return { ...item, completed: true };
+      }
+      return item;
+    });
+
+    setTodos(newTodosItem);
+  };
+
+  const deleteTodo = todoId => {
+    const newTodosItem = todos.filter(item => item.id != todoId);
+    setTodos(newTodosItem);
+  };
+
+  const clearAllTodos = () => {
+    Alert.alert('Confirm', 'Clear todos?', [
+      {
+        text: 'Yes',
+        onPress: () => setTodos([]),
+      },
+      {
+        text: 'No',
+      },
+    ]);
+  };
+
+  const ListItem = ({ todo }) => {
+    return (
+      <View style={styles.listItem}>
+        <View style={{ flex: 1 }}>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 15,
+              color: COLORS.primary,
+              textDecorationLine: todo?.completed ? 'line-through' : 'none',
+            }}>
+            {todo?.task}
+          </Text>
+        </View>
+        {!todo?.completed && (
+          <TouchableOpacity onPress={() => markTodoComplete(todo.id)}>
+            <View style={[styles.actionIcon, { backgroundColor: 'green' }]}>
+              <Icon name="done" size={20} color="white" />
+            </View>
+          </TouchableOpacity>
+        )}
+        <TouchableOpacity onPress={() => deleteTodo(todo.id)}>
+          <View style={styles.actionIcon}>
+            <Icon name="delete" size={20} color="white" />
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
+  //*🖼️Screen View*//
+  return (
+    <SafeAreaView           
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <View style={styles.header}>
+        <Text
+          style={{
+            fontWeight: 'bold',
+            fontSize: 20,
+            color: COLORS.primary,
+          }}>
+          TODO APP
+        </Text>
+        <Icon name="delete" size={25} color="red" onPress={clearAllTodos} />
+      </View>
+      <FlatList                                                             //*FlatList*//
+        showsVerticalScrollIndicator={false}                                    //스크롤 숨기기
+        contentContainerStyle={{ padding: 20, paddingBottom: 100 }}             //자식들 스타일
+        data={todos}                                                            //리스트의 source를 담는 prop
+        renderItem={({ item }) => <ListItem todo={item} />}                     //data로 받은 source 각각을 render시켜주는 콜백함수
+      />
+
+      <View style={styles.footer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={textInput}
+            placeholder="Add Todo"
+            onChangeText={text => setTextInput(text)}
+          />
+        </View>
+        <TouchableOpacity onPress={addTodo}>
+          <View style={styles.iconContainer}>
+            <Icon name="add" color="white" size={30} />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: COLORS.white,
+  },
+  inputContainer: {
+    height: 50,
+    paddingHorizontal: 20,
+    elevation: 40,
+    backgroundColor: COLORS.white,
+    flex: 1,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 30,
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: COLORS.primary,
+    elevation: 40,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  listItem: {
+    padding: 20,
+    backgroundColor: COLORS.white,
+    flexDirection: 'row',
+    elevation: 12,
+    borderRadius: 7,
+    marginVertical: 10,
+  },
+  actionIcon: {
+    height: 25,
+    width: 25,
+    backgroundColor: COLORS.white,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red',
+    marginLeft: 5,
+    borderRadius: 3,
+  },
+  header: {
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
+
+export default TodoApp;
